f126ae8e785a9ae2694f93878a8c23a19a1b0483
tamar2791 <tamizaltz@gmail.com>
2025-09-21 23:28:02 +0300

    update monitor

--- monitor.sv
+++ monitor.sv
@@ -1,34 +1,47 @@
-module monitor;
-   //-----
-   // Signal monitor
-   //----
-   reg [NUM_ITEMS-1:0] [7:0] parallel_in_mon;
-   int			     mon_item_cnt = 0;
-   
-   reg [NUM_ITEMS-1:0] [7:0] serial_in_mon;
-   int			     mon_cycle_cnt = 0;   
-   
-   initial begin
-      forever begin // monitor is always on. Never controls simulation execution
-	 @ (posedge clk);
-	 if (load == 1'b1) begin
-	    parallel_in_mon [mon_item_cnt] <= parallel_in;
-	 
-`ifdef MONITOR_LOG_ON
-	 $display ("TIME [%0t] : ITEM %0d : Received parallel in: %h", $time (), mon_item_cnt, parallel_in);	 
-`endif
-	 
-	    @ (posedge clk); // According to design spec, serial output starts on next cycle
-	    mon_cycle_cnt = 0;	 
-	    repeat (8) begin
-	       serial_in_mon [mon_item_cnt] [mon_cycle_cnt++] <= serial_out;
-`ifdef MONITOR_LOG_ON	        
-	       $display ("TIME [%0t] : Received serial out [%0d] = %b", $time (), (mon_cycle_cnt - 1),serial_out );	    
-`endif	    
-	       @ (posedge clk); // move to next serial output	    
-	    end
-	    ++mon_item_cnt;
-	 end // if (load == 1'b1)	 	 
-      end // forever begin      
-   end // initial begin   
-endmodule
+import p2s_pkg::*;
+
+class p2s_monitor;
+
+	virtual interface par_to_ser_if.MON vif;
+	mailbox #(p2s_txn_t) mbx_mon2sb;
+	int unsigned serial_len;
+
+	function new(virtual interface par_to_ser_if.MON vif,
+			mailbox #(p2s_txn_t) mbx);
+		this.vif = vif;
+		this.mbx_mon2sb = mbx;
+		this.serial_len = vif.SERIAL_LEN;
+	endfunction
+
+	task automatic reset_phase();
+		if(!vif.rst_n)
+		begin
+			wait(vif.rst_n == 1'b1);
+			repeat (2) @(vif.cb_mon);
+		end
+	endtask
+
+
+	task automatic run();
+		p2s_txn_t tr;
+
+		reset_phase();
+
+		forever begin
+			@(vif.cb_mon);
+
+			if(vif.cb_mon.load)
+			begin
+				tr.par = vif.cb_mon.parallel_in;
+				@(vif.cb_mon);
+				for(int i = 0; i < serial_len; i++)
+				begin
+					tr.ser[i] = vif.cb_mon.serial_out;
+					@(vif.cb_mon);
+				end
+				mbx_mon2sb.put(tr);
+			end
+		end
+	endtask
+
+endclass
