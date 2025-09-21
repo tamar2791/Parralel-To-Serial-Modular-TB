338df0510b9cea475cea94bcec27edfbdac0812f
tamar2791 <tamizaltz@gmail.com>
2025-09-21 18:29:50 +0300

    update dut

--- dut.sv
+++ dut.sv
@@ -1,15 +1,16 @@
-module parallel_to_serial
-(input clk,
-input load,
-input [7:0] parallel_in,
-output serial_out);
-reg [7:0] shift_register;
-always_ff @ (posedge clk) begin
-  for (int i = 0; i < 7; i++)
-     shift_register [i] <= load ? parallel_in [i] : shift_register [i+1];
-  shift_register [7] <= parallel_in [7];
- // A more “standard” approach would be (Why didn’t I do that?) :
- // shift_register [7:0] <= load ? parallel_in : {1’b0,shift_register [7:1]};
-end
-assign serial_out = shift_register [0];
+module parallel_to_serial (par_to_ser_if.DUT ifc);
+
+   logic [P2S_SERIAL_LEN-1:0] shift_register;
+
+   assign ifc.serial_out = shift_register[0];
+
+   always_ff @(posedge ifc.clk, negedge ifc.rst_n)
+   begin
+      if(!ifc.rst_n)
+         shift_register <= '0;
+      else if(ifc.load)
+         shift_register <= ifc.parallel_in;
+      else
+         shift_register <= {1'b0, shift_register[P2S_SERIAL_LEN-1:1]};
+   end
 endmodule
